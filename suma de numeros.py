3bb7b0fa6189974534461e28d231f4583dac2aa8
carloxx1 <90403818+carloxx1@users.noreply.github.com>
2021-09-29 20:54:43 -0500

    Delete suma de numeros.py

--- suma de numeros.py
+++
@@ -1,9 +0,0 @@
-def sumadenumeros(numeros):
-
-   if len(numeros) == 1:
-        return numeros[0]
-   else:
-        return numeros[0] + sumadenumeros(numeros[1:])
-lista=[2,3,4,1,5]
-resultado= sumadenumeros(lista)
-print(resultado)
